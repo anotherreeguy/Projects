af6a5391c39dc0d77a6aafc4fe8aa550b7e10195
anotherreeguy <168342264+anotherreeguy@users.noreply.github.com>
2025-03-08 01:36:24 +0800

    Create descriptions.js

---
+++ projects/mcskinstealer/descriptions.js
@@ -0,0 +1,11 @@
+const descriptions = {
+    "2535422415009551": "reeguy bedrock account reveal??!?",
+    "2535464959603122": "cool youtuber :)",
+    "67c543ca-7946-433a-bc82-4002921ff0b3": "testing"
+};
+
+const userColors = {
+    "2535464959603122": "grey", 
+    "2535422415009551": "purple",
+    "67c543ca-7946-433a-bc82-4002921ff0b3": "purple"
+};
