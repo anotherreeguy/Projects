2ae71da5a4727dd67ace61c1087afccbc6427ae0
anotherreeguy <168342264+anotherreeguy@users.noreply.github.com>
2025-06-01 22:42:43 +0800

    Update script.js

--- projects/mcskinstealer/script.js
+++ projects/mcskinstealer/script.js
@@ -28,6 +28,7 @@ async function fetchSkin() {
   downloadBtn.style.display = "none";
   historyEl.innerHTML = "";
   spinner.style.display = "block";
+  info.innerHTML = "";
 
   if (!username) {
     errorEl.textContent = "Please enter a valid username.";
@@ -42,11 +43,9 @@ async function fetchSkin() {
 
     if (data.error) throw new Error(data.error);
 
-    const textureId = currentEdition === "java"
-      ? data.texture_url.split("/").pop()
-      : data.skinTextureId;
-
+    const textureId = data.texture_id;
     const skinUrl = `https://textures.minecraft.net/texture/${textureId}`;
+
     preview.src = skinUrl;
     preview.style.display = "block";
     downloadBtn.style.display = "inline-block";
@@ -57,15 +56,17 @@ async function fetchSkin() {
       Current user: ${username}
     `;
 
-    const others = (data.users_with_this_skin || []).filter(name => name !== username);
-    if (others.length > 0) {
-      historyEl.innerHTML = "<p>Also used by:</p>";
-      for (const other of others) {
+    const allUsers = data.users_with_this_skin || [];
+    const previousUsers = allUsers.filter(name => name !== username);
+
+    if (previousUsers.length > 0) {
+      historyEl.innerHTML = "<p>Previously used by:</p>";
+      for (const name of previousUsers) {
         const thumb = document.createElement("img");
-        thumb.src = `https://minotar.net/armor/body/${other}/64`;
-        thumb.alt = other;
+        thumb.src = `https://minotar.net/armor/body/${name}/64`;
+        thumb.alt = name;
         thumb.className = "skin-thumb";
-        thumb.title = other;
+        thumb.title = name;
         historyEl.appendChild(thumb);
       }
     }
