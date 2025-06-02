79018fdc999629ec8d497c971415cf357984c22b
anotherreeguy <168342264+anotherreeguy@users.noreply.github.com>
2025-06-02 19:46:03 +0800

    Delete projects/mcskinstealer/descriptions.js

--- projects/mcskinstealer/descriptions.js
+++
@@ -1,5 +0,0 @@
-const descriptions = {
-    "2535422415009551": "reeguy bedrock account reveal??!?",
-    "2535464959603122": "cool youtuber :)",
-    "67c543ca-7946-433a-bc82-4002921ff0b3": "ReeGuy!"
-};
